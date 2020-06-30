460c8c564e248bb3c0ace698644ef36e31200f0e
yixuan WANG <yixuanamelia@gmail.com>
2020-06-30 18:02:29 +0200

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import {
 import Login from './components/pages/Login/Login';
 import Signup from './components/pages/Signup/Signup';
 import ForgotPass from './components/pages/ForgotPass/ForgotPass';
+import Main from './components/pages/Main/Mainpage';
 
 require('dotenv').config();
 
@@ -22,6 +23,8 @@ function App() {
           <Route exact path="/" component={Signup} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/forgotPass" component={ForgotPass} />
+          <Route exact path="/home" component={Main} />
+
         </Switch>
       </Router>
     </div>

d25b3b9d37de8bfa2ffe56df41a3085affcff163
yixuan WANG <yixuanamelia@gmail.com>
2020-09-03 20:38:16 +0200

    notification pop-up added

--- client/src/components/pages/common/Navbar/Navbar.jsx
+++ client/src/components/pages/common/Navbar/Navbar.jsx
@@ -3,6 +3,17 @@ import "./Navbar.css";
 
 
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      popVisible: false
+    }
+  }
+
+  showPopList = (e) => {
+    e.preventDefault();
+    this.setState({ popVisible: !this.state.popVisible })
+  }
 
   render() {
     return (
@@ -13,17 +24,67 @@ class Navbar extends React.Component {
             <a class="active" href="/">Home</a>
             <a href="/chatroom">Chatroom</a>
             <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-            <a href="/notifications" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a href="#" onClick={(e) => this.showPopList(e)}>
               <i style={{ fontSize: '22px' }} class="fa fa-bell"></i>
               <div class="notifnum">
                 <span>3</span>
               </div>
+              {this.state.popVisible === true ?
+                <div class="recpoint"></div> : null}
+
             </a>
 
             <a href="/editprofile">Me</a>
             <a href="/logout">Logout</a>
           </div>
         </div>
+
+        {this.state.popVisible === true ?
+          <div class="notiflist">
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+            <div class="singlenotif">
+              abn has liked your profile
+                  <span>12 jan 2019 at 10:03 pm</span>
+            </div>
+
+          </div>
+          : null}
+
       </>
     );
   };

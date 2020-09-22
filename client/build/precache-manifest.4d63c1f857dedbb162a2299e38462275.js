ca4408c834613f7f5652ebec6c0db26b924714e5
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-09-23 00:38:30 +0200

    BUILD : - New build

--- client/build/precache-manifest.4d63c1f857dedbb162a2299e38462275.js
+++ client/build/precache-manifest.4d63c1f857dedbb162a2299e38462275.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85d2f99927dde3845cdc5cb1c89aa92a",
+    "revision": "b3fc8ea828bcd0388caa6849d38a18fd",
     "url": "/index.html"
   },
   {
-    "revision": "96cac988a5349a1075b6",
+    "revision": "9ea8d3dfb07a2ec17de7",
     "url": "/static/css/2.6f9185ba.chunk.css"
   },
   {
-    "revision": "e67f70521b0e2dfb2917",
+    "revision": "a812d8a4f222bcdbcaba",
     "url": "/static/css/main.729b8eb0.chunk.css"
   },
   {
-    "revision": "96cac988a5349a1075b6",
-    "url": "/static/js/2.1108ba98.chunk.js"
+    "revision": "9ea8d3dfb07a2ec17de7",
+    "url": "/static/js/2.e79fa42a.chunk.js"
   },
   {
-    "revision": "e67f70521b0e2dfb2917",
-    "url": "/static/js/main.e3a1f8de.chunk.js"
+    "revision": "a812d8a4f222bcdbcaba",
+    "url": "/static/js/main.e54e6ce9.chunk.js"
   },
   {
     "revision": "912737604dc36bb63112",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58637e753f7a82275d3801e5ec0c3706.js"
+  "/precache-manifest.4d63c1f857dedbb162a2299e38462275.js"
 );
 
 self.addEventListener('message', (event) => {

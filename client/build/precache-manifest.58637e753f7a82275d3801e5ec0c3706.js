ea0cb34a5eae42b8b80ef227c80a8b901e24f471
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2020-09-23 00:07:16 +0200

    BUILD : - New build

---
+++ client/build/precache-manifest.58637e753f7a82275d3801e5ec0c3706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d2f99927dde3845cdc5cb1c89aa92a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96cac988a5349a1075b6",
+    "url": "/static/css/2.6f9185ba.chunk.css"
+  },
+  {
+    "revision": "e67f70521b0e2dfb2917",
+    "url": "/static/css/main.729b8eb0.chunk.css"
+  },
+  {
+    "revision": "96cac988a5349a1075b6",
+    "url": "/static/js/2.1108ba98.chunk.js"
+  },
+  {
+    "revision": "e67f70521b0e2dfb2917",
+    "url": "/static/js/main.e3a1f8de.chunk.js"
+  },
+  {
+    "revision": "912737604dc36bb63112",
+    "url": "/static/js/runtime-main.1aa75da1.js"
+  },
+  {
+    "revision": "50418d06978a2beb9d6ea6acbbe06ee0",
+    "url": "/static/media/bg-01.50418d06.jpg"
+  }
+]);
